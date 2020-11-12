1d23db8240e0f26ebf0bf36c6a071872e387661a
jaehoon <qs9875@gmail.com>
2020-11-12 13:55:01 +0900

    chap03

--- src/main/java/com/jh9875/book/springboot/Application.java
+++ src/main/java/com/jh9875/book/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-    //test
+    //test2
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
