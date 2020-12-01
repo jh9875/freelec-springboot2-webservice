0b28aa3793ed8759960c682be759e6acc21fe317
jaehoon <qs9875@gmail.com>
2020-12-01 14:49:40 +0900

    merge conflict error check

---
+++ src/main/java/com/jh9875/book/springboot/web/IndexController.java
@@ -0,0 +1,43 @@
+package com.jh9875.book.springboot.web;
+
+import com.jh9875.book.springboot.config.auth.LoginUser;
+import com.jh9875.book.springboot.config.auth.dto.SessionUser;
+import com.jh9875.book.springboot.service.posts.PostsService;
+import com.jh9875.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+//    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user !=null) {
+            model.addAttribute("nameOfUser", user.getName());
+            System.out.println(user.getName());
+        }
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto =postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
